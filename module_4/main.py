7348455937efef80bcf13d0f38fcd9594f1d854c
ahmedsha <shakil.ahmed@engelvoelkers.com>
2026-07-25 18:41:26 +0200

    Weather & Currency Functions Added

--- module_4/main.py
+++ module_4/main.py
@@ -1,13 +1,37 @@
 import sys
-from library import Library
-from book import Book
-from member import Member
 
-# Create an instance
-# member1 = Member("Alice", 30)
-member1 = Member("Alice",30,1,"Python Programming")
-print(member1.display_info())
+import requests
 
+# - - - - - - - Current Weather - - - - - - - - -
+def weather():
+    url = "https://wttr.in/Dhaka?format=j1"
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        print(response.json())
+    else:
+        print("Failed to retrieve data:", response.status_code) 
+
+
+
+# - - - - - - - Currency Exchange Rate - - - - - - - - -
+
+def currency():
+    url = "https://open.er-api.com/v6/latest/USD"
+    alt_url = "https://api.exchangerate-api.com/v4/latest/USD"
+
+    response = requests.get(url)
+    alt_response = requests.get(alt_url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        print(response.json())
+    elif alt_response.status_code == 200:
+            print("Alt:- - - - - - - -")
+            print(response.json())
+    else:
+        print("Failed to retrieve data:", response.status_code) 
 
 def display_menu():
     print("\n===================================================================")
@@ -17,19 +41,17 @@ def display_menu():
     print("# 2. Currency Exchange Rate")
     print("# 3. Save Result to JSON File")
     print("# 4. View Previous Saved Data")
-    print("# 8. Exit")
+    print("# 5. Exit")
 
 def main():
-    libary = Library()
     while True:
         try:
             display_menu()
             choice= int(input("Enter An Option: "))
             if choice==1:
-                pass
+                weather()
             elif choice==2:
-                pass
-                
+                currency()                
             elif choice==3:
                 pass
             elif choice==4:
